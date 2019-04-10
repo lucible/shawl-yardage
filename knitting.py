73f09529838f1e148a6e72e6eadce0f63468e092
lucible <wave@lucible.xyz>
2019-04-09 22:37:42 -0500

    added two basic predicting functions

--- knitting.py
+++ knitting.py
@@ -37,6 +37,26 @@ print(f'{round(avgRate, 5)} / {round(avgRate, 3)}\n')
 
 futureWeight = initialWeight * exp(decayRates[-1] * (len(rowWeights) + 3))
 
+predicted_3_ahead = []
+
+for index, decay in enumerate(decayRates):
+    predicate = initialWeight * exp(decay * (index + 4))
+    if index < len(rowWeights) - 3:
+        pairs = (round(predicate, 2), rowWeights[index + 3])
+        predicted_3_ahead.append(pairs)
+
+print(predicted_vs_actual)
+
+predicted_3_ahead = []
+
+for index, decay in enumerate(decayRates):
+    predicate = initialWeight * exp(decay * (index + 2))
+    if index < len(rowWeights) - 1:
+        pairs = (round(predicate, 2), rowWeights[index + 1])
+        predicted_1_ahead.append(pairs)
+
+print(predicted_1_ahead)
+
 print('Predicted Weight After Next 3 Rows:')
 print(f'{round(futureWeight, 3)} grams\n')
 
